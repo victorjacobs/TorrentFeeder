1915e6bebda5c5dc3679e05789de58f4bc34d9a0
Victor Jacobs <vikking@gmail.com>
2011-01-25 12:16:45 +0100

    Added GPLv3 statements in all source files.

--- include/feedhandler.class.php
+++ include/feedhandler.class.php
@@ -1,5 +1,22 @@
 <?php
 	
+	/*    This file is part of TorrentFeeder
+	 *    Copyright (C) 2011  Victor Jacobs
+	 *
+	 *    TorrentFeeder is free software: you can redistribute it and/or modify
+	 *    it under the terms of the GNU Affero General Public License as
+	 *    published by the Free Software Foundation, either version 3 of the
+	 *    License, or (at your option) any later version.
+	 *
+	 *    TorrentFeeder is distributed in the hope that it will be useful,
+	 *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *    GNU Affero General Public License for more details.
+	 *
+	 *    You should have received a copy of the GNU Affero General Public License
+	 *    along with TorrentFeeder.  If not, see <http://www.gnu.org/licenses/>.
+	 */
+	
 	/*
 	 * FeedHandler builds up an RSS Feed using PHP's DOM functions
 	*/
